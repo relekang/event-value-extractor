bbdee89042398ab46dc8103b5d81390fa2dc7fed
Rolf Erik Lekang <me@rolflekang.com>
2016-01-07 23:11:57 +0100

    chore: Convert tests to ava :sparkles:

---
+++ test.js
@@ -0,0 +1,25 @@
+import 'babel-core/register'
+import test from 'ava'
+import sinon from 'sinon'
+
+import * as eve from './index'
+
+const testValueExtraction = (t, extractor, value, outValue) => {
+  const spy = sinon.spy()
+  extractor(spy)({target: {name: 'fieldName', value: value}})
+  t.ok(spy.called)
+  t.same(spy.args[0], ['fieldName', outValue || value])
+}
+
+test('value(func, parser, parserArguments) should return the value in the target object', t => {
+  testValueExtraction(t, eve.value, 'hi there')
+})
+
+test('intValue(func) should return an parsed int of the value in the target object', t => {
+  testValueExtraction(t, eve.intValue, '42', 42)
+  testValueExtraction(t, eve.intValue, '3.14', 3)
+})
+
+test('floatValue(func) should return an parsed float of  the value in the target object', t => {
+  testValueExtraction(t, eve.floatValue, '3.14', 3.14)
+})
