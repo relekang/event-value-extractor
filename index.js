f598331579629c36adc64aa7e57d2a5f09978f55
Rolf Erik Lekang <me@rolflekang.com>
2016-01-07 16:59:08 +0100

    chore: Add eslint config

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 export function value(func, parser, parserArgs) {
   return ({target}) => {
     if (parser) {
-      return func(target.name, parser.apply(this, [target.value].concat(parserArgs)));
+      return func(target.name, parser.apply(this, [target.value].concat(parserArgs)))
     }
-    return func(target.name, target.value);
-  };
+    return func(target.name, target.value)
+  }
 }
 
 export function intValue(func) {
-  return value(func, parseInt, 10);
+  return value(func, parseInt, 10)
 }
 
 export function floatValue(func) {
-  return value(func, parseFloat, 10);
+  return value(func, parseFloat, 10)
 }
--- tests.js
+++ tests.js
@@ -1,34 +1,34 @@
 /* eslint-env mocha */
-import chai from 'chai';
-import sinonChai from 'sinon-chai';
-import sinon from 'sinon';
+import chai from 'chai'
+import sinonChai from 'sinon-chai'
+import sinon from 'sinon'
 
-import * as eve from './index';
+import * as eve from './index'
 
-chai.use(sinonChai);
-const {expect} = chai;
+chai.use(sinonChai)
+const {expect} = chai
 
 const testValueExtraction = (extractor, value, outValue) => {
-  const spy = sinon.spy();
-  extractor(spy)({target: {name: 'fieldName', value: value}});
-  expect(spy).to.have.been.calledWith('fieldName', outValue || value);
-};
+  const spy = sinon.spy()
+  extractor(spy)({target: {name: 'fieldName', value: value}})
+  expect(spy).to.have.been.calledWith('fieldName', outValue || value)
+}
 
 describe('value(func, parser, parserArguments)', () => {
   it('should return the value in the target object', () => {
-    testValueExtraction(eve.value, 'hi there');
-  });
-});
+    testValueExtraction(eve.value, 'hi there')
+  })
+})
 
 describe('intValue(func)', () => {
   it('should return an parsed int of the value in the target object', () => {
-    testValueExtraction(eve.intValue, '42', 42);
-    testValueExtraction(eve.intValue, '3.14', 3);
-  });
-});
+    testValueExtraction(eve.intValue, '42', 42)
+    testValueExtraction(eve.intValue, '3.14', 3)
+  })
+})
 
 describe('floatValue(func)', () => {
   it('should return an parsed float of  the value in the target object', () => {
-    testValueExtraction(eve.floatValue, '3.14', 3.14);
-  });
-});
+    testValueExtraction(eve.floatValue, '3.14', 3.14)
+  })
+})
